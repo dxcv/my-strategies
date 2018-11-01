5cb81403efaf4f5d0a159e5806913ccfc3a2ec25
jijunnan <ji_junnan@163.com>
2018-11-01 17:39:09 +0800

    插入数据库
    Excel2DB.insert()
    Excel2DB.update1()

--- strategy1/database.py
+++ strategy1/database.py
@@ -16,20 +16,19 @@ import re
 
 def create_database(cur):
     """本函数用于创建数据库与表"""
-    _ = cur.execute("create database if not exists strategy1 character set UTF8")
+    _ = cur.execute("create database if not exists strategy1 character set UTF8MB4")
     _ = cur.execute("use strategy1")
     sql_tb_pri = """
     create table if not exists tb_pri(
     `dt` date NOT NULL COMMENT '发行日期',
     `code` char(15) NOT NULL PRIMARY KEY COMMENT '债券代码',
-    `name` char(15) NOT NULL COMMENT '债券名称',
     `term` int NOT NULL COMMENT '债券期限',
-    `rate` float(6, 4) COMMENT '中标利率',
+    `rate` float(6, 4) DEFAULT NULL COMMENT '中标利率',
     `mg_rate` float(7, 4) DEFAULT NULL COMMENT '边际中标利率',
-    `multiplier` float(4, 2) NOT NULL COMMENT '中标倍数',
-    `mg_multiplier` float(4, 2) DEFAULT NULL COMMENT '边际中标倍数',
-    `bondtype` char(15) NOT NULL COMMENT '债券类型'
-    )ENGINE=InnoDB DEFAULT CHARSET = utf8 COMMENT = '从wind一级发行专题中下载的数据'
+    `multiplier` float(5, 2) DEFAULT NULL COMMENT '中标倍数',
+    `mg_multiplier` float(5, 2) DEFAULT NULL COMMENT '边际中标倍数',
+    `bondtype` char(15) DEFAULT "国债" COMMENT '债券类型'
+    )ENGINE=InnoDB DEFAULT CHARSET = utf8MB3 COMMENT = '从wind一级发行专题中下载的数据'
     """
     _ = cur.execute(sql_tb_pri)
 
@@ -38,14 +37,14 @@ def create_database(cur):
     `dt` date NOT NULL COMMENT '招标日期',
     `code` char(15) NOT NULL PRIMARY KEY COMMENT '债券代码',
     `term` int NOT NULL COMMENT '债券期限',
-    `amount` float(6, 2) NOT NULL COMMENT '债券发行量',
+    `amount` float(6, 2) DEFAULT NULL COMMENT '债券发行量',
     `rate` float(6, 4) NOT NULL COMMENT '加权利率',
     `mg_rate` float(6, 4) DEFAULT NULL COMMENT '边际利率',
-    `multiplier` float(4, 2) DEFAULT NULL COMMENT '全场倍数',
-    `mg_multiplier` float(4, 2) DEFAULT NULL COMMENT '边际倍数',
-    `dt_pay` date NOT NULL COMMENT '缴款日',
-    `dt_list` date NOT NULL COMMENT '上市日'
-    )ENGINE=InnoDB DEFAULT CHARSET = utf8 COMMENT = '从QB中下载的数据'
+    `multiplier` float(5, 2) DEFAULT NULL COMMENT '全场倍数',
+    `mg_multiplier` float(5, 2) DEFAULT NULL COMMENT '边际倍数',
+    `dt_pay` date DEFAULT NULL COMMENT '缴款日',
+    `dt_list` date DEFAULT NULL COMMENT '上市日'
+    )ENGINE=InnoDB DEFAULT CHARSET = utf8MB3 COMMENT = '从QB中下载的数据'
     """
     _ = cur.execute(sql_appendix1)
 
@@ -134,11 +133,11 @@ class ReadExcel(object):
         init_pattern = re.compile(r"\d{2}00\d{2}[^xX]+")
         data = self.ws.Range(self.ws.Cells(2,1), self.ws.Cells(2,31).End(4)).Value
         # 首发国债数据
-        init = [[d[2].strftime("%Y-%m-%d"), d[0], d[1], d[4], d[29], d[10], self.multipliers(d[20], 2),
-                 self.mg_multipliers(d[14], d[15]), d[30]] for d in data if re.match(init_pattern, d[0])]
+        init = [([d[2].strftime("%Y-%m-%d"), d[0], d[4], d[29], d[10], self.multipliers(d[20], 2),
+                 self.mg_multipliers(d[14], d[15]), d[30]], ) for d in data if re.match(init_pattern, d[0])]
         # 续发国债数据
-        cont = [[d[2].strftime("%Y-%m-%d"), d[0], d[1], d[4], d[27], d[13], self.multipliers(d[20], 2),
-                 self.mg_multipliers(d[14], d[15]), d[30]] for d in data if re.match(cont_pattern, d[0])]
+        cont = [([d[2].strftime("%Y-%m-%d"), d[0], d[4], d[27], d[13], self.multipliers(d[20], 2),
+                 self.mg_multipliers(d[14], d[15]), d[30]], ) for d in data if re.match(cont_pattern, d[0])]
         init.extend(cont)
         return init
 
@@ -146,24 +145,30 @@ class ReadExcel(object):
         """从利率债发行结果中提取需要的数据"""
         p = re.compile(r"\d{2}附息国债")
         data = self.ws.Range(self.ws.Cells(3, 1), self.ws.Cells(3, 12).End(4)).Value
-        res = [[self.cdt2dt(d[0]), self.name2code(d[2]), self.term2int(d[3]), d[4], d[5], d[5], d[6], d[7], d[8],
-                self.cdt2dt(d[10]), self.cdt2dt(d[11])] for d in data if re.match(p, d[2])]
+        res = [([self.cdt2dt(d[0]), self.name2code(d[2]), self.term2int(d[3]), d[4], d[5], d[6], d[7],
+                 self.qb_mg_multipliers(d[8]), self.cdt2dt(d[10]), self.cdt2dt(d[11])], )
+               for d in data if re.match(p, d[2])]
         return res
 
     def cdt2dt(self, cdt):
-        """将中文的日期改为标准格式的日期
-        例如01月11日添加上年份成为2018-1-11"""
+        """将中文的日期改为标准格式的日期，例如01月11日添加上年份成为2018-1-11"""
         p = re.compile(r"\d{2}")
-        res = re.findall(p, cdt)
-        dt = [str(self.year), res[0], res[1]]
-        return "-".join(dt)
+        if cdt is None:
+            return None
+        else:
+            res = re.findall(p, cdt)
+            dt = [str(self.year), res[0], res[1]]
+            return "-".join(dt)
 
     @staticmethod
     def mg_multipliers(a, b):
         if a is None or b is None:
             return None
         else:
-            return round(a/b, 2)
+            res = round(a/b, 2)
+            if res>10:  # 当边际倍数大于10时，该指标的意义就不大了
+                res = 10
+            return res
 
     @staticmethod
     def multipliers(a, b):
@@ -173,6 +178,15 @@ class ReadExcel(object):
             return round(a, b)
 
     @staticmethod
+    def qb_mg_multipliers(a):
+        if a is None:
+            return None
+        elif float(a) > 10:
+            return 10
+        else:
+            return a
+
+    @staticmethod
     def name2code(name):
         """根据国债名称获得债券代码"""
         ym_p = re.compile(r"\d{2}")
@@ -180,7 +194,10 @@ class ReadExcel(object):
         res_ym = re.findall(ym_p, name)
         res_x = re.search(x_p, name)
         if res_x:
-            res = res_ym[0]+"00"+res_ym[1]+res_x.group()+".IB"
+            if res_x.group() == "X1":
+                res = res_ym[0] + "00" + res_ym[1] + "X" + ".IB"
+            else:
+                res = res_ym[0]+"00"+res_ym[1]+res_x.group()+".IB"
         else:
             res = res_ym[0]+"00"+res_ym[1]+".IB"
         return res
@@ -199,27 +216,82 @@ class Excel2DB(object):
     """本类用于从Excel中读取数据后写入数据库"""
     def __init__(self, data_path, db):
         self.data_path = data_path  # 存放excel文件的路径
-        self.xlapp = win32com.client.Dispatch("Excel.Application")
+        self.xlapp = win32com.client.gencache.EnsureDispatch("Excel.Application")
         self.db = db
         self.cur = self.db.cursor()
 
-    def insert(self, bondtype, year):
+    def insert1(self, bondtype, year):
+        """按表格类型与年份将单张excel表格写入数据库"""
         rd = ReadExcel(bondtype, year, self.data_path, self.xlapp)
         data = rd.extract()
         if bondtype == "国债":
             table = "tb_pri"
         elif bondtype == "QB补充":
             table = "appendix1"
+        else:
+            table = None
         sql = "insert into {} values %s".format(table)
-        self.cur.executemany(sql, data)
+        try:
+            _ = self.cur.executemany(sql, data)
+        except pymysql.err.IntegrityError as e:
+            self.db.rollback()
+            print(bondtype, year, e)
+        else:
+            self.db.commit()
 
+    def insert2(self, dt1='2017-7-29', dt2='2017-11-21'):
+        """从wind数据库上下载的招投标结果缺失了2017年7月29日至11月21日之间的数据，可从QB的表格内补充该部分数据至tb_pri"""
+        sql = """insert into tb_pri(dt, code, term, rate, mg_rate, multiplier, mg_multiplier) 
+              select dt, code, term, rate, mg_rate, multiplier, mg_multiplier from appendix1 
+              where dt between %s and %s"""
+        self.cur.execute(sql, (dt1, dt2))
+        self.db.commit()
 
+    def insert(self, years):
+        create_database(self.cur)
+        for year in years:
+            self.insert1("国债", year)
+            self.insert1("QB补充", year)
+        if 2017 in years:
+            self.insert2()
+
+    def update1(self, mode=0):
+        """由于从WIND下载的17年7月之后的招投标结果缺少边际利率，全场倍数与边际倍数，因此可以使用QB的数据来进行补充
+        可以对三个字段分别更新或者一起更新（mode=0)"""
+        sql1 = """update tb_pri t inner join appendix1 a on t.code = a.code 
+        set t.multiplier = a.multiplier where t.multiplier is NULL"""
+        sql2 = """update tb_pri t inner join appendix1 a on t.code = a.code
+        set t.mg_multiplier = a.mg_multiplier where t.mg_multiplier is NULL"""
+        sql3 = """update tb_pri t inner join appendix1 a on t.code = a.code
+        set t.mg_rate = a.mg_rate where t.mg_rate is NULL"""
+        try:
+            if mode == 0:
+                self.cur.execute(sql1)
+                self.cur.execute(sql2)
+                self.cur.execute(sql3)
+            else:
+                self.cur.execute(eval("sql{}".format(mode)))
+        except:
+            print("Excel2DB.update1出现错误")
+            self.db.rollback()
+        else:
+            self.db.commit()
+
+    def close(self):
+        self.cur.close()
 
 
 def main():
-    w.start()
     db = pymysql.connect("localhost", "root", "root", charset="utf8")
     data_path = r"f:\reports\my report\report1\数据"  # excel数据文件存放路径
+    years = range(2013, 2019)
+    try:
+        e2db = Excel2DB(data_path, db)
+        e2db.insert(years)
+    finally:
+        e2db.close()
+        db.close()
+
 
 # class Wind2DB(object):
 #     """本类用于从Wind中读取数据并写入数据库"""
@@ -341,6 +413,7 @@ def main():
 #     """画图函数"""
 #     with DBPlot(db) as dbplot:
 #         dbplot.delta_plot("10Y", "国债")
+
 
 if __name__ == "__main__":
     main()
