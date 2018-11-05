57bc261f67f7e6cf49b9218e9b90a0b47a5d025a
jijunnan <ji_junnan@163.com>
2018-11-05 17:35:19 +0800

    国开债
    增加国开债数据

--- strategy1/database.py
+++ strategy1/database.py
@@ -73,7 +73,7 @@ class Data(object):
 
 class BondYTM(object):
     """本类用于计算续发固定利率附息国债到期收益率"""
-    def __init__(self, terms, rate, dt0, par=100, freq=1):
+    def __init__(self, terms, rate, dt0: dtt.date, par=100, freq=1):
         """类初始化函数，terms代表债券年限，rate表示发行利率，dt0表示发行日期，par表示债券面值，默认100
         freq表示一年附息频次，默认为1"""
         self.terms = terms
@@ -85,17 +85,17 @@ class BondYTM(object):
     def get_ts(self, dt: dtt.date):
         dt_delta_days = (dt - self.dt0).days
         year_days = 365
-        t0 = round(dt_delta_days * self.freq / year_days, 2)
+        t0 = 1 - dt_delta_days * self.freq / year_days
         ts = [i for i in range(self.terms*self.freq)]
         return t0, ts
 
-    def bond_ytm(self, dt, price, guess=0.03):
+    def bond_ytm(self, dt:dtt.date, price, guess=0.03):
         t0, ts = self.get_ts(dt)
         coup = self.par * self.rate / self.freq
         ytm_func = lambda y: (sum([coup / (1 + y / self.freq) ** t for t in ts]) + self.par / (1 + y / self.freq) **
                               ts[-1]) / (1 + t0 * y / self.freq) - price
-        fprime = lambda y: (sum([-t * coup / self.freq * (1 + y / self.freq) ** (t + 1) for t in ts]) - ts[
-            -1] * self.par / self.freq * (1 + y / self.freq) ** (ts[-1] + 1)) / (
+        fprime = lambda y: (sum([-t * coup / self.freq / (1 + y / self.freq) ** (t + 1) for t in ts]) - ts[
+            -1] * self.par / self.freq / (1 + y / self.freq) ** (ts[-1] + 1)) / (
                                        1 + t0 * y / self.freq) - t0 / self.freq * (1 + t0 * y / self.freq) ** (-2) * (
                                        sum([coup / (1 + y / self.freq) ** t for t in ts]) + self.par / (
                                            1 + y / self.freq) ** ts[-1])
@@ -124,7 +124,7 @@ class ReadExcel(object):
         elif self.bondtype == "QB补充":
             res = self.__data2()
         elif self.bondtype == "国开债":
-            res = None
+            res = self.__data3()
         return res
 
     def __data1(self):
@@ -142,13 +142,27 @@ class ReadExcel(object):
         return init
 
     def __data2(self):
-        """从利率债发行结果中提取需要的数据"""
+        """从利率债发行结果中提取需要的附息国债的数据"""
         p = re.compile(r"\d{2}附息国债")
         data = self.ws.Range(self.ws.Cells(3, 1), self.ws.Cells(3, 12).End(4)).Value
         res = [([self.cdt2dt(d[0]), self.name2code(d[2]), self.term2int(d[3]), d[4], d[5], d[6], d[7],
                  self.qb_mg_multipliers(d[8]), self.cdt2dt(d[10]), self.cdt2dt(d[11])], )
                for d in data if re.match(p, d[2])]
         return res
+
+    def __data3(self):
+        """从国开债招投标结果中提取国开债的数据"""
+        init_pattern = re.compile(r"\d{2}02\d{2}[^Z]+", re.I)
+        cont_pattern = re.compile(r"\d{2}02\d{2}Z+", re.I)
+        data = self.ws.Range(self.ws.Cells(2, 1), self.ws.Cells(2, 31).End(4)).Value
+        # 首发国开债数据
+        init = [([d[2].strftime("%Y-%m-%d"), d[0], d[4], d[29], d[29], self.multipliers(d[20], 2),
+                  self.mg_multipliers(d[14], d[15]), d[30]],) for d in data if re.match(init_pattern, d[0])]
+        # 续发国开债数据
+        cont = [([d[2].strftime("%Y-%m-%d"), d[0], d[4], d[27], d[27], self.multipliers(d[20], 2),
+                  self.mg_multipliers(d[14], d[15]), d[30]],) for d in data if re.match(cont_pattern, d[0])]
+        init.extend(cont)
+        return init
 
     def cdt2dt(self, cdt):
         """将中文的日期改为标准格式的日期，例如01月11日添加上年份成为2018-1-11"""
@@ -219,6 +233,7 @@ class Excel2DB(object):
         self.xlapp = win32com.client.Dispatch("Excel.Application")
         self.db = db
         self.cur = self.db.cursor()
+        create_database(self.cur)
 
     def insert1(self, bondtype, year):
         """按表格类型与年份将单张excel表格写入数据库"""
@@ -228,6 +243,8 @@ class Excel2DB(object):
             table = "tb_pri"
         elif bondtype == "QB补充":
             table = "appendix1"
+        elif bondtype == "国开债":
+            table = "tb_pri"
         else:
             table = None
         sql = "insert into {} values %s".format(table)
@@ -248,7 +265,6 @@ class Excel2DB(object):
         self.db.commit()
 
     def insert(self, years):
-        create_database(self.cur)
         for year in years:
             self.insert1("国债", year)
             self.insert1("QB补充", year)
@@ -290,7 +306,18 @@ class Excel2DB(object):
         # 利用边际利率是否大于50来判断该字段的记录是价格还是利率，当大于50时一般对应的时价格，因为利率很难超过50%
         sql_select = """select t1.dt, t1.code, t1.term, t1.rate, t1.mg_rate, t2.dt, t2.code, t2.rate
                      from tb_pri t1, tb_pri t2 where t2.code = concat(left(t1.code, 6), ".IB") and t1.mg_rate > 50"""
-        # TODO
+        data = Data(sql_select, self.cur).data
+        # for d in data:
+        #     print(d[2], d[7], d[5], d[0], d[4], d[1], end="     ")
+        #     print(BondYTM(d[2], d[7], d[5]).bond_ytm(d[0], d[4]))
+        data_update = [[BondYTM(d[2], d[7], d[5]).bond_ytm(d[0], d[4]), d[1]] for d in data]
+        sql_update = """update tb_pri set mg_rate = %s where code = %s"""
+        try:
+            _ = self.cur.executemany(sql_update, data_update)
+        except:
+            self.db.rollback()
+        else:
+            self.db.commit()
 
     def close(self):
         self.cur.close()
@@ -304,131 +331,10 @@ def main():
     try:
         e2db.insert(years)
         e2db.update()
+        e2db.update_mg_rate()
     finally:
         e2db.close()
         db.close()
-
-
-# class Wind2DB(object):
-#     """本类用于从Wind中读取数据并写入数据库"""
-#     def __init__(self, db, bondtype, market="secondary"):
-#         self.db = db
-#         self.cur = self.db.cursor()
-#         if market == "primary":
-#             if bondtype == "国债":
-#                 self.data = w.edb("M1001940,M1001942,M1001943,M1001944,M1001946", "2013-01-01", "2018-06-14")
-#                 self.table = "tb_pri"
-#             elif bondtype == "国开债":
-#                 self.data = w.edb("M1004440,M1004441,M1004442,M1004443,M1004444", "2013-01-01", "2018-06-14")
-#                 self.table = "cdb_pri"
-#             else:
-#                 raise IndexError("错误的bondtype参数类型")
-#         elif market == "secondary":
-#             if bondtype == "国债":
-#                 self.data = w.edb("S0059744,S0059746,S0059747,S0059748,S0059749", "2013-01-01", "2018-06-14")
-#                 self.table = "tb_sec"
-#             elif bondtype == "国开债":
-#                 self.data = w.edb("M1004263,M1004265,M1004267,M1004269,M1004271", "2013-01-01", "2018-06-14")
-#                 self.table = "cdb_sec"
-#             else:
-#                 raise IndexError("错误的bondtype参数类型")
-#         else:
-#             raise IndexError("错误的market参数类型")
-#
-#     def insert(self):
-#         sql = "insert into {} values %s".format(self.table)
-#         try:
-#             for dt, y1, y2, y3, y4, y5 in zip(self.data.Times, self.data.Data[0], self.data.Data[1], self.data.Data[2],
-#                                               self.data.Data[3], self.data.Data[4]):
-#                 s = []
-#                 for y in (dt, y1, y2, y3, y4, y5):
-#                     if isinstance(y, dtt.date):
-#                         s.append(y)
-#                     elif math.isnan(y):
-#                         s.append(None)
-#                     else:
-#                         s.append(y)
-#                 self.cur.execute(sql, (s,))
-#             self.db.commit()
-#         except Exception as e:
-#             self.db.rollback()
-#             print(s)
-#             print(e)
-#
-#
-# def create_table(db, bondtype, market="issue_result"):
-#     if market == "issue_result":
-#         excel2db = Excel2DB(db, bondtype)
-#         excel2db.insert()
-#         excel2db.close()
-#     elif market == "primary":
-#         wind2db = Wind2DB(db, bondtype, market="primary")
-#         wind2db.insert()
-#     elif market == "secondary":
-#         wind2db = Wind2DB(db, bondtype)
-#         wind2db.insert()
-#     else:
-#         raise IndexError("错误的market参数类型")
-#
-#
-# class DBPlot(object):
-#     """本类用于使用数据库的数据进行画图"""
-#     def __init__(self, db):
-#         self.db = db
-#         self.cur = self.db.cursor()
-#
-#     def close(self):
-#         self.cur.close()
-#
-#     def __enter__(self):
-#         return self
-#
-#     def __exit__(self, exc_type, exc_val, exc_tb):
-#         self.close()
-#
-#     def get_deltadata(self, term, bondtype):
-#         if bondtype == "国债":
-#             table1 = "tb_pri"
-#             table2 = "tb_sec"
-#         elif bondtype == "国开债":
-#             table1 = "cdb_pri"
-#             table2 = "cdb_sec"
-#         else:
-#             raise IndexError("错误的参数bondtype")
-#         sql1 = "select dt, {0} from {1} where {0} is not null".format(term, table1)
-#         sql2 = "select dt, {0} from {1} ".format(term, table2)
-#         df1 = pd.read_sql(sql1, self.db, "dt")
-#         df2 = pd.read_sql(sql2, self.db, "dt")
-#         df3 = df2.diff()
-#         index_num = np.array([i-1 for i in range(len(df2)) if df2.index[i] in df1.index])
-#         df4 = pd.DataFrame(df1.iloc[:, 0].values-df2.iloc[index_num, 0].values, index=df1.index)
-#         return df1, df2, df3, df4
-#
-#     def delta_plot(self, term, bondtype):
-#         """本方法用于绘制三合一的图，分别是二级市场利率走势图，一级市场发行当日二级市场到期收益率变动，以及一级市场
-#         发行结果相对上一交易日二级市场收盘价的变动"""
-#         df1, df2, df3, df4 = self.get_deltadata(term, bondtype)
-#         figure, axes = plt.subplots(3, 1, True)
-#         axes[0].plot(df2)
-#         axes[1].vlines(df3.loc[df4.index].index, 0, df3.loc[df4.index])
-#         axes[2].vlines(df4.index, 0, df4)
-#         plt.show()
-#
-#
-# def create_db(db):
-#     """创建数据库内的表格"""
-#     # create_table(db, "国债")
-#     create_table(db, "国开债")
-#     # create_table(db, "国债", "secondary")
-#     # create_table(db, "国开债", "secondary")
-#     # create_table(db, "国债", "primary")
-#     # create_table(db, "国开债", "primary")
-#
-#
-# def figures(db):
-#     """画图函数"""
-#     with DBPlot(db) as dbplot:
-#         dbplot.delta_plot("10Y", "国债")
 
 
 if __name__ == "__main__":
